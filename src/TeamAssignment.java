cef11fc216f8c108fdc648cb4b805e4b518cc248
hevaltasdemir <heval.tasdemir@gmail.com>
2021-04-03 16:35:02 -0400

    printValues requiment done

--- src/TeamAssignment.java
+++ src/TeamAssignment.java
@@ -4,36 +4,39 @@ import java.util.*;
 public class TeamAssignment {
 
     // method to print out all values in a List<String> void printValues(List<String> list) {} Heval
-    static List<String>  printValues(List<String> list) {
+    static List<String> printValues(List<String> list) {
+        List<String> printInputs = new ArrayList<>();
 
+        for (String i : list) {
+            printInputs.add(i);
+        }
+        return printInputs;
     }
-
 
 
     // method to add all the numbers in a List<Integer>: int sum(List<Integer> list) {} Heval
 
 
-
     // method to find the index of a value in a String[]: int getIndex(String[] arr, String value) {} Beritan
-
 
 
     // method to multiply two int together: int multiply(int a, int b) {} İbo
 
 
-
     // method to tell if an int is even: boolean isEven(int number) {} Serap
 
 
-
     // method to print hello world: void helloWorld() {} Hardi
-    public void hellowWorld(){
+    public void hellowWorld() {
         System.out.println("Hellow World");
     }
 
     public static void main(String[] args) {
 
         //You guys can test your codes in this main method. You guys dont need to creat new main method again.
+
+        List<String> strings = Arrays.asList("test1","test2","test3");
+        System.out.println(printValues(strings));
 
 
     }
