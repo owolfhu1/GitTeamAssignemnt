702734061fff785a6253c1dc1a5e377fdfac99e7
hevaltasdemir <heval.tasdemir@gmail.com>
2021-04-03 14:01:38 -0400

    assignmented team members

--- src/TeamAssignment.java
+++ src/TeamAssignment.java
@@ -2,26 +2,26 @@ import java.util.*;
 
 public class TeamAssignment {
 
-    // method to print out all values in a List<String> void printValues(List<String> list) {}
+    // method to print out all values in a List<String> void printValues(List<String> list) {} Heval
 
 
-    // method to add all the numbers in a List<Integer>: int sum(List<Integer> list) {}
-
-
-
-    // method to find the index of a value in a String[]: int getIndex(String[] arr, String value) {}
+    // method to add all the numbers in a List<Integer>: int sum(List<Integer> list) {} Heval
 
 
 
-    // method to multiply two int together: int multiply(int a, int b) {}
+    // method to find the index of a value in a String[]: int getIndex(String[] arr, String value) {} Beritan
 
 
 
-    // method to tell if an int is even: boolean isEven(int number) {}
+    // method to multiply two int together: int multiply(int a, int b) {} İbo
 
 
 
-    // method to print hello world: void helloWorld() {}
+    // method to tell if an int is even: boolean isEven(int number) {} Serap
+
+
+
+    // method to print hello world: void helloWorld() {} Hardi
 
 
 
